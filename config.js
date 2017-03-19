b79b5d2323308ac33c12c8acf9c6fb07c3ccdad1
Vladimir Zeifman <edoomik@gmail.com>
2017-03-19 22:11:22 +0200

    updated config.js with configuration related to code transpiling, running phases

--- config.js
+++ config.js
@@ -7,6 +7,27 @@ exports.PORT = PORT;
 exports.HOSTNAME = HOSTNAME;
 exports.CODE_EXECUTION_TIMEOUT_MS = CODE_EXECUTION_TIMEOUT_MS;
 exports.CODE_COMPILE_TIMEOUT_MS = CODE_COMPILE_TIMEOUT_MS;
+exports.VM_SCRIPT_OPTIONS = {
+    filename: 'your-code.js',
+    timeout: CODE_COMPILE_TIMEOUT_MS
+};
+
+exports.VM_RUN_IN_CONTEXT_OPTIONS = {
+    filename: 'your-code.js',
+    timeout: CODE_EXECUTION_TIMEOUT_MS
+};
+
+exports.TYPESCRIPT_TRANSPILER_OPTIONS = {
+    compilerOptions: {
+        oImplicitUseStrict: true,
+        module: 'ES6'
+    }
+};
+
+exports.COFFEESCRIPT_TRANSPILER_OPTIONS = {
+    bare: true
+};
+
 exports.MODULES_DIR = '../modules/node_modules';
 exports.moduleToAlias = {
     'faker': 'faker',
@@ -18,3 +39,4 @@ exports.languages = [
     {name: 'coffeescript', enabled: true},
     {name: 'typescript', enabled: true}
 ];
+
